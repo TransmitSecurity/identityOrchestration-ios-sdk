a3989b31445a8d175d9c18f3ef950a870cb35af7
Igor Babitski <igor.babitski@transmitsecurity.com>
2023-09-26 17:03:52 +0300

    version 0.0.2

--- Package.swift
+++ Package.swift
@@ -14,7 +14,7 @@ let package = Package(
             targets: ["IdentityOrchestration", "IdentityOrchestration-Dependencies"])
     ],
     dependencies: [
-        .package(url: "https://github.com/TransmitSecurity/core-ios-sdk.git", from: "1.0.13")
+        .package(url: "https://github.com/TransmitSecurity/core-ios-sdk.git", from: "1.0.14")
     ],
     targets: [
         .binaryTarget(
